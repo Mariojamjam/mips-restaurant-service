603224c396cc4bc2d097d90be7260e3169b1b738
MarioJam <mj.andrade.moreira@gmail.com>
2026-05-02 02:37:22 -0300

    feat: added strlib .asm files

---
+++ src/strlib/strcat.asm
@@ -0,0 +1,30 @@
+strcat:
+        #Copy destination address to temp pointer
+        move $t0, $a0
+strcat_find_end:
+        #Load current byte from destination
+        lb $t1, 0($t0)
+        #If '\0' found, start copying here
+        beq $t1, $zero, strcat_copy
+        #Advance destination pointer
+        addi $t0, $t0, 1
+        #Continue searching for end
+        j strcat_find_end
+strcat_copy:
+        #Load current byte from source
+        lb $t2, 0($a1)
+        #Write byte into destination
+        sb $t2, 0($t0)
+        #If null terminator copied, done
+        beq $t2, $zero, strcat_final
+        #Advance source pointer
+        addi $a1, $a1, 1
+        #Advance destination pointer
+        addi $t0, $t0, 1
+        #Repeat copy loop
+        j strcat_copy
+strcat_final:
+        #Return original destination address
+        move $v0, $a0
+        #Return to caller
+        jr $ra
---
+++ src/strlib/strcmp.asm
@@ -0,0 +1,26 @@
+strcmp:
+strcmp_loop:
+        #Load current byte from str1
+        lb $t0, 0($a0)
+        #Load current byte from str2
+        lb $t1, 0($a1)
+        #If characters differ, go to diff
+        bne $t0, $t1, strcmp_diff
+        #If both are '\0', strings are equal
+        beq $t0, $zero, strcmp_equal
+        #Advance str1 pointer
+        addi $a0, $a0, 1
+        #Advance str2 pointer
+        addi $a1, $a1, 1
+        #Repeat loop
+        j strcmp_loop
+strcmp_diff:
+        #Return difference between characters
+        sub $v0, $t0, $t1
+        #Return to caller
+        jr $ra
+strcmp_equal:
+        #Return 0 (strings are equal)
+        li $v0, 0
+        #Return to caller
+        jr $ra
