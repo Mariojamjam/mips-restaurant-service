982a8d7383e31562e04e709e0018383b89d15939
Mariojamjam <mj.andrade.moreira@gmail.com>
2026-05-06 16:31:57 -0300

    fix: added comments explaining the function

--- src/data_management/save_all_data.asm
+++ src/data_management/save_all_data.asm
@@ -1,91 +1,149 @@
+#This function aims to save all restaurant data into a binary file.
+#It opens the save file in write mode, writes a small header with metadata,
+#then writes the full menus block and the full tables block to the file.
+#
+#The file structure written by this function is:
+#       4 bytes: save header signature
+#       4 bytes: menu block size
+#       4 bytes: table block size
+#       N bytes: menus block
+#       N bytes: tables block
+#
+#Input:
+#       save_file_name: file name used to store the binary data
+#       save_header_signature: identifier used to validate the save file
+#       save_header_menu_size: expected size of the menus block
+#       save_header_table_size: expected size of the tables block
+#       menus: menu items array stored in memory
+#       tables: tables array stored in memory
+#
+#Output:
+#       The restaurant data is written into the save file
+#       A success message is printed if the data was saved correctly
+#       An error message is printed if the file could not be opened or written
+
 .data
-    save_success_message: .asciiz "Data saved successfully"
-    save_error_message:   .asciiz "Error saving data"
+        save_success_message: .asciiz "Data saved successfully"
+        save_error_message:   .asciiz "Error saving data"
 
 .text
-
 save_all_data:
-	addi $sp, $sp, -8
+        #Opening stack space to preserve the return address and the file descriptor
+        #Stack layout:
+        #       0($sp): saved $ra
+        #       4($sp): file descriptor
+        addi $sp, $sp, -8
         sw   $ra, 0($sp)
 
-        # Open file: restaurant.bin
+        #Opening the save file in write mode
+        #Syscall 13 receives:
+        #       $a0: file name address
+        #       $a1: file open flag
+        #       $a2: file mode
         li   $v0, 13
         la   $a0, save_file_name
-        li   $a1, 1              # write
+        li   $a1, 1
         li   $a2, 0
         syscall
-	
-	bltz $v0, save_error
-	
-	sw   $v0, 4($sp)
-	
-	li   $v0, 15
+
+        #If the returned file descriptor is negative, the file could not be opened
+        bltz $v0, save_error
+
+        #Saving the file descriptor on the stack because it will be reused in multiple syscalls
+        sw   $v0, 4($sp)
+
+        #Writing the save file signature
+        #This signature can be used later to identify if the file is a valid save file
+        li   $v0, 15
         lw   $a0, 4($sp)
         la   $a1, save_header_signature
         li   $a2, 4
         syscall
-        
+
+        #If the write syscall returns a negative value, close the file and report an error
         bltz $v0, save_close_error
-        
-        # Write header menu size
+
+        #Writing the menu block size stored in the save header
+        #This can be used later to validate how many bytes must be loaded for the menus block
         li   $v0, 15
         lw   $a0, 4($sp)
         la   $a1, save_header_menu_size
         li   $a2, 4
         syscall
-        
+
+        #If the write syscall returns a negative value, close the file and report an error
         bltz $v0, save_close_error
 
-        # Write header table size
+        #Writing the table block size stored in the save header
+        #This can be used later to validate how many bytes must be loaded for the tables block
         li   $v0, 15
         lw   $a0, 4($sp)
         la   $a1, save_header_table_size
         li   $a2, 4
         syscall
-        
+
+        #If the write syscall returns a negative value, close the file and report an error
         bltz $v0, save_close_error
 
-        # Write menus block
+        #Writing the full menus block to the save file
+        #The block is written directly from the menu array memory area
         li   $v0, 15
         lw   $a0, 4($sp)
         la   $a1, menus
         li   $a2, MENU_TOTAL_BYTES
         syscall
-        
+
+        #If the write syscall returns a negative value, close the file and report an error
         bltz $v0, save_close_error
 
-        # Write tables block
+        #Writing the full tables block to the save file
+        #The block is written directly from the tables array memory area
         li   $v0, 15
         lw   $a0, 4($sp)
         la   $a1, tables
         li   $a2, TABLE_TOTAL_BYTES
         syscall
-        
+
+        #If the write syscall returns a negative value, close the file and report an error
         bltz $v0, save_close_error
 
-        # Close file
+        #Closing the save file after all data was written
         li   $v0, 16
         lw   $a0, 4($sp)
         syscall
 
-        # Success message
+        #Printing the success message after saving all data
         la   $a0, save_success_message
         jal  print_str_mmio
 
+        #Restoring the return address from the stack
         lw   $ra, 0($sp)
+
+        #Closing the stack space used by this function
         addi $sp, $sp, 8
+
+        #Returning to the caller
         jr   $ra
 
+
 save_close_error:
-        # Try to close file before leaving on write failure
+        #Closing the save file before leaving after a write error
+        #This avoids leaving the file descriptor open
         li   $v0, 16
         lw   $a0, 4($sp)
         syscall
-        
+
+
 save_error:
+        #Printing the generic save error message
         la   $a0, save_error_message
         jal  print_str_mmio
 
+        #Restoring the return address from the stack
         lw   $ra, 0($sp)
+
+        #Closing the stack space used by this function
         addi $sp, $sp, 8
+
+        #Returning to the caller
         jr   $ra
