b27a64e045f214f1b822c739509fd4f398dad67c
MjwsantiagoN <134097753+MjwsantiagoN@users.noreply.github.com>
2026-05-06 16:41:24 -0300

    Add table start to commands_table

--- src/commands_table.asm
+++ src/commands_table.asm
@@ -11,6 +11,7 @@
 	pt_menu_rm: .asciiz "menu_rm"
 	pt_menu_list: .asciiz "menu_list"
 	pt_menu_format: .asciiz "menu_format"
+	pt_table_start: .asciiz "mesa_iniciar"
 	
 	#==== MESSAGE FOR INVALID COMMNADS ====#
 	msg_invalid: .asciiz "Invalid Command"
@@ -26,6 +27,7 @@ commands_table:
 	.word pt_menu_rm, menu_rm, 7
 	.word pt_menu_list, menu_list, 9
 	.word pt_menu_format, menu_format, 11
+	.word pt_table_start, table_start, 12
 	
 	#Used for comparison, defining the end of the table.
 	.word 0, 0, 0                
