2e49be15d77cd2c7675a2f9d095da3f4b45e9681
MarioJam <mj.andrade.moreira@gmail.com>
2026-05-07 03:56:47 -0300

    feat: register table report and payment commands
    Includes partial_table, table_format and table_pay in the main assembly graph.
    
    Registers the three commands in the shell dispatch table so they can be executed from terminal input.

--- src/commands_table.asm
+++ src/commands_table.asm
@@ -12,6 +12,9 @@
 	pt_menu_list: .asciiz "menu_list"
 	pt_menu_format: .asciiz "menu_format"
 	pt_order_add: .asciiz "order_add"
+	pt_partial_table: .asciiz "partial_table"
+	pt_table_format: .asciiz "table_format"
+	pt_table_pay: .asciiz "table_pay"
 	pt_table_rm_item: .asciiz "table_rm_item"
 	pt_save_all_data: .asciiz "save_all_data"
 	pt_load_all_data: .asciiz "load_all_data"
@@ -33,6 +36,9 @@ commands_table:
 	.word pt_menu_list, menu_list, 9
 	.word pt_menu_format, menu_format, 11
 	.word pt_order_add, order_add, 9
+	.word pt_partial_table, partial_table, 13
+	.word pt_table_format, table_format, 12
+	.word pt_table_pay, table_pay, 9
 	.word pt_table_rm_item,table_rm_item, 13 
 	.word pt_save_all_data, save_all_data, 13
 	.word pt_load_all_data, load_all_data, 13
