2a0ae0205cc832fcb514e94124740959ab294533
MarioJam <mj.andrade.moreira@gmail.com>
2026-05-07 03:14:49 -0300

    feat: register order_add command in dispatch table
    Adds the order_add command name and target routine to the shell dispatch table.
    
    This allows the command parser to route terminal input to the new order workflow.

--- src/commands_table.asm
+++ src/commands_table.asm
@@ -11,6 +11,7 @@
 	pt_menu_rm: .asciiz "menu_rm"
 	pt_menu_list: .asciiz "menu_list"
 	pt_menu_format: .asciiz "menu_format"
+	pt_order_add: .asciiz "order_add"
 	pt_table_rm_item: .asciiz "table_rm_item"
 	pt_save_all_data: .asciiz "save_all_data"
 	pt_load_all_data: .asciiz "load_all_data"
@@ -31,6 +32,7 @@ commands_table:
 	.word pt_menu_rm, menu_rm, 7
 	.word pt_menu_list, menu_list, 9
 	.word pt_menu_format, menu_format, 11
+	.word pt_order_add, order_add, 9
 	.word pt_table_rm_item,table_rm_item, 13 
 	.word pt_save_all_data, save_all_data, 13
 	.word pt_load_all_data, load_all_data, 13
